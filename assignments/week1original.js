8233995480049edad2c2e1259e493e67590e4342
Nellie <tene23kl@student.ju.se>
2025-09-23 20:19:15 +0200

    week1

---
+++ assignments/week1original.js
@@ -0,0 +1,81 @@
+//Recreation of the original work Dialogue between emotions by Vera Molnar
+function setup() {
+    createCanvas(innerWidth, innerHeight);
+}
+
+
+const size = 100;//size of each element
+const layers = 6; //number of rows/ layers in each element
+
+const colors = [
+     [240,24,24],//red
+     [104, 167, 186], //lightblue
+     [255, 255, 255],    
+     [65, 138, 17],//green
+     [194, 114, 166],//pink
+     [255, 255, 0] //yellow
+ ];
+//function manipulate the different layers random, adds a random variant.
+function getRandomValue(pos, variance) { //takes a position
+    return pos + map(Math.random(), 0,1,- variance, variance); //map, maps from one system to a different system. We get back a random number between 0-1
+}
+
+  
+//Function to draw the single element
+function drawLayers(x, y ,size, layers) {
+    let colorIndex = Math.floor(Math.random() * colors.length);//Picks a random number (0, 1, 2, 3, 4, or 5) to choose which color from the array
+    let chosenColor = colors[colorIndex];                        // Get the color array at that index
+    stroke(chosenColor[0], chosenColor[1], chosenColor[2]);     // Use R, G, B values
+    strokeWeight(3); 
+    // const half = size / 2;
+    //define a variance
+    const variance = size / 7; // here varianve is 10 pixels for every direction
+    noFill();
+    //rectMode(CENTER); This would center all rectangles within eachother
+    for(let i = 0; i < layers; i++) { //A for loop. Add 1 layer until 10
+        if(Math.random()> 0.9) { //Randomize look
+            continue;
+        }      
+        const s = (size / layers) * i; //make the rectangle smaller. s= size, original size
+        const half = s / 2;
+        beginShape();
+        vertex(
+            getRandomValue(x - half, variance), 
+            getRandomValue(y - half, variance)
+        );//creates layers on upper side
+        
+        vertex(
+            getRandomValue(x + half, variance), 
+            getRandomValue(y - half, variance)
+        );//Creates layers on right side
+        
+        vertex(
+            getRandomValue(x + half, variance), 
+            getRandomValue(y + half, variance)
+        );//creates layers on under side
+        
+        vertex(getRandomValue(x - half, variance),
+        getRandomValue(y + half, variance)
+    );
+        
+    endShape(CLOSE);//closes the shape
+        // rect(x-half, y-half, s, s);// draw rectangle. (x,y,width,height)
+    }
+}
+
+
+
+function draw() {
+    background(0,0,0); 
+
+    // drawLayers(100, 100, size, layers);
+
+    //Now we want to put the rectangle with layers in a grid, just as the original artwork. This is done with a for loop
+    for(let y = 0; y < 10; y++) {
+        for(let x = 0; x <10; x++) {
+            drawLayers(size / 2 + x * size, size / 2 + y * size, size, layers);
+        }
+    }
+
+    noLoop();
+}
