519f39ca300a479dc7f0f612c8bc128be2cbeec9
Nellie <tene23kl@student.ju.se>
2025-09-23 22:46:47 +0200

    georgnees

--- assignments/week1_nees_og.js
+++ assignments/week1_nees_og.js
@@ -1,9 +1,49 @@
-//Schotter By Georg Nees (1968)
-//My attempt on recreating the original artpiece
+// //Schotter By Georg Nees (1968)
+// //My attempt on recreating the original artpiece
+// const size = 60;
+// const gap = 0;
+// const amountX = 5;
+// const amountY = 7;
+
+// function setup() {
+//   createCanvas(innerWidth, innerHeight);
+// }
+
+// function draw() {
+//   background(255, 255, 255);
+//   noFill();
+//   stroke(0, 0, 0);
+//   strokeWeight(1);
+
+//   let y = (height - size * amountY - gap * (amountY - 1)) / 2;
+//   let rotation = 0;
+
+//   for (let i = 0; i < amountY; i++) {
+//     let x = (width - size * amountX - gap * (amountX - 1)) / 2;
+
+//     let baseRotation = (i / amountY) * 0.5;
+
+//     for (let k = 0; k < amountX; k++) {
+//       push();
+//       translate(x, y);
+
+//       let randomRotation = random (- baseRotation, baseRotation);
+//       rotate(randomRotation);
+      
+//       square(0, 0, size);
+//       pop();
+//       x += size + gap;
+//     //   rotation += 0.02;
+//     }
+//     y += size + gap;
+//   }
+
+//   noLoop();
+// }
+
 const size = 60;
-const gap = 0;
-const amountX = 5;
-const amountY = 7;
+const gap = 20;
+const amount = 5;
 
 function setup() {
   createCanvas(innerWidth, innerHeight);
@@ -13,27 +53,20 @@ function draw() {
   background(255, 255, 255);
   noFill();
   stroke(0, 0, 0);
-  strokeWeight(1);
+  strokeWeight(2);
 
-  let y = (height - size * amountY - gap * (amountY - 1)) / 2;
+  let y = (height - size * amount - gap * (amount - 1)) / 2;
   let rotation = 0;
-
-  for (let i = 0; i < amountY; i++) {
-    let x = (width - size * amountX - gap * (amountX - 1)) / 2;
-
-    let baseRotation = (i / amountY) * 0.5;
-
-    for (let k = 0; k < amountX; k++) {
+  for (let i = 0; i < amount; i++) {
+    let x = (width - size * amount - gap * (amount - 1)) / 2;
+    for (let k = 0; k < amount; k++) {
       push();
       translate(x, y);
-
-      let randomRotation = random (- baseRotation, baseRotation);
-      rotate(randomRotation);
-      
+      rotate(rotation);
       square(0, 0, size);
       pop();
       x += size + gap;
-    //   rotation += 0.02;
+      rotation += 0.02;
     }
     y += size + gap;
   }
