32c20a57d1ef1c427a678af2fa8b05736cbdf912
Nellie <tene23kl@student.ju.se>
2025-10-01 11:40:15 +0200

    change location folder

--- experiments/week4_tone.js
+++ experiments/week4_tone.js
@@ -19,7 +19,7 @@ function getRandomValue(pos, variance, isUnorganized) { //takes a position
     const varianceReduction = isUnorganized ? 1:8;//Less reduction for unorganized squares
     // The next line of code was retreived from p5js website. https://p5js.org/reference/p5/map/
     return pos + map(Math.random(), 0,1,- variance/varianceReduction, variance/varianceReduction); //map, maps from one system to a different system. We get back a random number between 0-1. I reduce the variance number by 8 to create an ordered appearence
-} 
+}
 
 
 //Function to draw the single element
